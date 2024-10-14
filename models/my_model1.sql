2495bb2bc47600a39dccb0cab0a0e8d895e34799
gamershere3@gmail.com <gamershere3@gmail.com>
2024-10-14 11:35:30 +0000

    ff

--- models/my_model1.sql
+++ models/my_model1.sql
@@ -1,7 +1,22 @@
-CREATE OR REPLACE TABLE my_filtered_data AS (
+-- models/my_model.sql
+
+CREATE OR REPLACE airflow.DEMO_PUBLIC.my_model1 AS (
     SELECT
-        *
+        period,
+        respondent,
+        respondent_name,
+        fueltype,
+        type_name,
+        value,
+        value_units
     FROM
         electricity_data
-    LIMIT 10
+    WHERE
+        period IS NOT NULL AND
+        respondent IS NOT NULL AND
+        respondent_name IS NOT NULL AND
+        fueltype IS NOT NULL AND
+        type_name IS NOT NULL AND
+        value IS NOT NULL AND
+        value_units IS NOT NULL
 );
