30c0c4ab7b058d8ddc4c4902da7ae93e0bda0a27
gamershere3@gmail.com <gamershere3@gmail.com>
2024-10-14 11:33:30 +0000

    commit

--- models/my_model1.sql
+++ models/my_model1.sql
@@ -1,20 +1,7 @@
 CREATE OR REPLACE TABLE my_filtered_data AS (
     SELECT
-        period,
-        respondent,
-        respondent_name,
-        fueltype,
-        type_name,
-        value,
-        value_units
+        *
     FROM
         electricity_data
-    WHERE
-        period IS NOT NULL AND
-        respondent IS NOT NULL AND
-        respondent_name IS NOT NULL AND
-        fueltype IS NOT NULL AND
-        type_name IS NOT NULL AND
-        value IS NOT NULL AND
-        value_units IS NOT NULL
+    LIMIT 10
 );
