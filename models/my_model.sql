6b33729be6313dc9710c5c1e4dd750eb0c9e46d8
gamershere3@gmail.com <gamershere3@gmail.com>
2024-10-14 10:52:07 +0000

    Add my_model transformation

---
+++ models/my_model.sql
@@ -0,0 +1,18 @@
+SELECT
+    period,
+    respondent,
+    respondent_name,
+    fueltype,
+    type_name,
+    value,
+    value_units
+FROM
+    electricity_data
+WHERE
+    period IS NOT NULL AND
+    respondent IS NOT NULL AND
+    respondent_name IS NOT NULL AND
+    fueltype IS NOT NULL AND
+    type_name IS NOT NULL AND
+    value IS NOT NULL AND
+    value_units IS NOT NULL;
