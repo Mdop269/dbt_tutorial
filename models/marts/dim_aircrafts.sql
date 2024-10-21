da78f65188b25f77866b3d7706e55aae7c0e6e4b
md op <gamershere3@gmail.com>
2024-10-21 06:16:23 +0000

    comit

--- models/marts/dim_aircrafts.sql
+++ models/marts/dim_aircrafts.sql
@@ -12,17 +12,27 @@
 
 -- SELECT * FROM aircraft_data;
 
--- models/marts/dim_aircrafts.sql
-{{ config(materialized='view') }}
+-- -- models/marts/dim_aircrafts.sql
+-- {{ config(materialized='view') }}
+
+-- WITH aircraft_data AS (
+--     SELECT
+--         DISTINCT aircraft_code,  -- No quotes, Snowflake assumes uppercase
+--         model,
+--         range
+--     FROM 
+--         airflow.cdc_public.aircrafts_data
+-- )
+
+-- SELECT * FROM aircraft_data;
 
 WITH aircraft_data AS (
-    SELECT
-        DISTINCT "aircraft_code",  -- If the column is lowercase, you need quotes around it
-        "model",
-        "range"
+    SELECT DISTINCT
+        aircraft_code,
+        model,
+        range
     FROM 
-        airflow.cdc_public.aircrafts_data  -- Ensure the schema and table name are correct
+        airflow.cdc_public.aircrafts_data
 )
 
 SELECT * FROM aircraft_data;
-
