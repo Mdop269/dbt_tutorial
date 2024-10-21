b30d1a60f7a9e9bf6d277344d1fb2387dee76569
md op <gamershere3@gmail.com>
2024-10-21 10:09:59 +0000

    c

--- models/marts/dim_aircrafts.sql
+++ models/marts/dim_aircrafts.sql
@@ -1,4 +1,4 @@
-/* models/marts/dim_aircrafts.sql  */
+
 
 
 {{ config(materialized='view') }}
@@ -6,19 +6,3 @@
 select distinct aircraft_code, model, range
 from {{ source("cdc_public", "aircrafts_data") }}  -- Ensure the table name is correct
 
-
-
-
-/* models/marts/dim_aircrafts.sql */
-/* {{ config(materialized='view') }} */
-/*
-with aircraft_data1 as (
-    select distinct aircraft_code, model, range
-    from {{ source("cdc_public", "aircrafts_data") }}  -- Ensure the table name is correct
-)
-
-select *
-from aircraft_data;  -- Add this line to select data from the CTE
-
-
-*/
