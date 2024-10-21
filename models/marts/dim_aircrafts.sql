c0bd95e249fe3afa2489c6d487576c9f48208f64
md op <gamershere3@gmail.com>
2024-10-21 05:54:59 +0000

    commit

--- models/marts/dim_aircrafts.sql
+++ models/marts/dim_aircrafts.sql
@@ -1,13 +1,28 @@
+-- -- models/marts/dim_aircrafts.sql
+-- {{ config(materialized='view') }}
+
+-- WITH aircraft_data AS (
+--     SELECT
+--         DISTINCT "aircraft_code",
+--         "model",
+--         "range"
+--     FROM 
+--         {{ source('cdc_public', 'aircrafts_data') }}  -- Ensure the table name is correct
+-- )
+
+-- SELECT * FROM aircraft_data;
+
 -- models/marts/dim_aircrafts.sql
-{{ config(materialized='view') }}
+{{ config(materialized='table') }}
 
 WITH aircraft_data AS (
     SELECT
-        DISTINCT "aircraft_code",
+        DISTINCT "aircraft_code",  -- If the column is lowercase, you need quotes around it
         "model",
         "range"
     FROM 
-        {{ source('cdc_public', 'aircrafts_data') }}  -- Ensure the table name is correct
+        airflow.cdc_public.aircrafts_data  -- Ensure the schema and table name are correct
 )
 
 SELECT * FROM aircraft_data;
+
