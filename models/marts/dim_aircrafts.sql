87c3098f1b0ded537c9319042eec059872d764e4
md op <gamershere3@gmail.com>
2024-10-21 07:41:40 +0000

    commit

--- models/marts/dim_aircrafts.sql
+++ models/marts/dim_aircrafts.sql
@@ -1,12 +1,15 @@
--- models/marts/dim_aircrafts.sql
-{{ config(materialized='view') }}
+/*models/marts/dim_aircrafts.sql 
+-- {{ config(materialized='view') }}
 
-WITH aircraft_data AS (
-    SELECT
-        DISTINCT aircraft_code,
-        model,
-        range
-    FROM 
-        {{ source('cdc_public', 'aircrafts_data') }}  -- Ensure the table name is correct
-)
+-- WITH aircraft_data AS (
+--     SELECT
+--         DISTINCT aircraft_code,
+--         model,
+--         range
+--     FROM 
+--         {{ source('cdc_public', 'aircrafts_data') }}  -- Ensure the table name is correct
+-- )
 
+*/ 
+SELECT count(*)
+FROM {{ source('cdc_public', 'aircrafts_data') }};
