bb00bc36a3eabe8474ec2e1381aecf79c733a3bb
md op <gamershere3@gmail.com>
2024-10-21 12:21:48 +0000

    c

--- models/marts/dim_flights.sql
+++ models/marts/dim_flights.sql
@@ -1,15 +1,15 @@
 {{ config(materialized='view')}}
 SELECT
     tf.flight_id,
-    flight_no,
-    scheduled_departure,
-    scheduled_arrival,
-    departure_airport,
-    arrival_airport,
-    status,
-    aircraft_code
+    f.flight_no,
+    f.scheduled_departure,
+    f.scheduled_arrival,
+    f.departure_airport,
+    f.arrival_airport,
+    f.status,
+    f.aircraft_code
 FROM
-    airflow.cdc_public.flights
+    airflow.cdc_public.flights As f
 JOIN 
     airflow.cdc_public.ticket_flights As tf ON f.flight_no = tf.flight_no 
                                        AND f.scheduled_departure = tf.scheduled_departure
