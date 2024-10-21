9e49652e6c415852f6cd502665389746b1a9a644
md op <gamershere3@gmail.com>
2024-10-21 06:33:24 +0000

    c

--- models/marts/dim_aircrafts.sql
+++ models/marts/dim_aircrafts.sql
@@ -1,38 +1,12 @@
--- -- models/marts/dim_aircrafts.sql
--- {{ config(materialized='view') }}
-
--- WITH aircraft_data AS (
---     SELECT
---         DISTINCT "aircraft_code",
---         "model",
---         "range"
---     FROM 
---         {{ source('cdc_public', 'aircrafts_data') }}  -- Ensure the table name is correct
--- )
-
--- SELECT * FROM aircraft_data;
-
--- -- models/marts/dim_aircrafts.sql
--- {{ config(materialized='view') }}
-
--- WITH aircraft_data AS (
---     SELECT
---         DISTINCT aircraft_code,  -- No quotes, Snowflake assumes uppercase
---         model,
---         range
---     FROM 
---         airflow.cdc_public.aircrafts_data
--- )
-
--- SELECT * FROM aircraft_data;
+-- models/marts/dim_aircrafts.sql
+{{ config(materialized='view') }}
 
 WITH aircraft_data AS (
-    SELECT DISTINCT
-        aircraft_code,
+    SELECT
+        DISTINCT aircraft_code,
         model,
         range
     FROM 
-        airflow.cdc_public.aircrafts_data
+        {{ source('cdc_public', 'aircrafts_data') }}  -- Ensure the table name is correct
 )
 
-SELECT * FROM aircraft_data;
